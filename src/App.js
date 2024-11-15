3a2bf916b8e7e044769c867fdb5781451343723a
Mukund Mittal <mukund.mukulmittal@gmail.com>
2024-11-15 17:30:39 +0530

    Games Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import LandingPage from './components/LandingPage';
-import HomePage from './components/HomePage';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import AboutPage from './components/AboutPage';
+import CoinQuest from './components/CoinQuest';
+import DonkeyKong from './components/DonkeyKong';
+import HomePage from './components/HomePage';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Pacman from './components/Pacman';
+import Signup from './components/Signup';
+import Snake from './components/Snake';
+import SpaceInvaders from './components/SpaceInvaders';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/landing" element={<LandingPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/landing" element={<LandingPage />} />
+				<Route path="/home" element={<HomePage />} />
+				<Route path="/about" element={<AboutPage />} />
+				{/* Games */}
+				<Route path="/coin-quest" element={<CoinQuest />} />
+				<Route path="/donkey-kong" element={<DonkeyKong />} />
+				<Route path="/pacman" element={<Pacman />} />
+				<Route path="/snake" element={<Snake />} />
+				<Route path="/space-invaders" element={<SpaceInvaders />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/SpaceInvaders/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+function SpaceInvadersGame() {
+	return (
+		<div className="home-page">
+			<nav className="navbar">
+				<h1 className="navbar-title">VibeQuest</h1>
+				<ul className="navbar-links">
+					<li>
+						<Link to="/home">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
+			</nav>
+
+			{/* Games Section */}
+			<section className="games-section">
+				<h2>Space Invaders</h2>
+				<div className="games-container">
+					<iframe
+						src="https://mukund1606.github.io/vibe-quest/space-invaders"
+						id="game-iframe"
+						title="Coin Quest"
+					></iframe>
+				</div>
+			</section>
+
+			{/* Footer */}
+			<footer className="footer">
+				<p>&copy; 2024 VibeQuest. All Rights Reserved.</p>
+			</footer>
+		</div>
+	);
+}
+
+export default SpaceInvadersGame;
